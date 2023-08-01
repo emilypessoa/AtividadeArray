ae3e293fbe3c78d70edd3ae599de7422fe53e358
Emily Pessoa <emymi2007@gmail.com>
2023-08-01 08:33:00 -0300

    criação do html, js e css em construção.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AtividadeArray
-Criação de um sistema que armazena nomes utilizando o array.
+Problema: Esquecer de fazer alguma tarefa é comum, porém você pode resolver isso! Utilizando uma lista de tarefas, podendo adicionar, excluir e editar, no final do dia você finaliza como concluida.
+
+Criação de um sistema de armazenamento de nomes utilizando o array.
+
---
+++ controller.js
@@ -0,0 +1,35 @@
+
+// FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY
+
+var dadosLista = [];
+function salvarTarefa(){
+    let nomeTarefa=document.getElementById("nomeTarefa").value;
+    if(nomeTarefa){
+        dadosLista.push(nomeTarefa);
+        criarLista();
+        document.getElementById('nomeTarefa').value = '';
+    }
+}
+
+// FUNÇÃO PARA CRIAR LISTA DE USUÁRIO
+
+function criarLista(){
+let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+for(let i = 0;i <= (dadosLista.length - 1);i++){
+    tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger'  onclick='finalizar(this.parentNode.parentNode.rowIndex)'>Finalizar</button></td></tr>";
+    document.getElementById('tabela').innerHTML = tabela;
+    }
+}
+
+// FUNÇÃO PARA EDITAR NOME
+
+function editar(i){
+    document.getElementById('nomeTarefa').value = dadosLista [(i - 1)];
+    dadosLista.splice(dadosLista[(i-1)], 1);
+}
+
+// FUNÇÃO PARA EXCLUIR NOME
+function finalizar(i){
+    dadosLista.splice((i - 1), 1);
+    document.getElementById('tabela').deleteRow(i)
+}
